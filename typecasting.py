cf0287357a5dd333aa9bdf19231b2796fba024bc
Umeen Rathnayake <umeenbg16@gmail.com>
2023-11-24 19:17:11 +0530

    variables and typecasting

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+#two types
+#1. Explicit
+#2. Implicit
+
+name = "Cap"
+age = 24
+gpa = 3.5
+student = True
+
---
+++ variables.py
@@ -0,0 +1,20 @@
+#Variables
+age = 24
+gpa = 3.5
+name = "Cap"
+online = False
+
+#print("You are " + str(age) + " years old")
+# same as java but you have to convert this to string
+
+#print("You are", age, "years old")
+
+#f-string
+print(f"You are {age} years old")
+print(f"Your gpa is {gpa}")
+print(f"Your name is {name}")
+print(f"Is your game is running: {online}")
+
+#data types
+#same as every programming language(duh)
+
