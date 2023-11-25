c3387d05471ede55654785df3a0ff317be9f80e4
Umeen Rathnayake <umeenbg16@gmail.com>
2023-11-25 10:24:39 +0530

    user inputs

---
+++ areacal.py
@@ -0,0 +1,6 @@
+length = float(input("Enter length of a rectangle: "))
+width = float(input("Enter width of a rectangle:"))
+
+area = length * width
+
+print(f"Area={area}")
--- typecasting.py
+++ typecasting.py
@@ -1,9 +1,42 @@
 #two types
-#1. Explicit
-#2. Implicit
+#1. Explicit(manual)
+#2. Implicit(automatically)
 
 name = "Cap"
 age = 24
 gpa = 3.5
 student = True
+
+#int to float
+age = float(age)
+print(age)
+
+#float to int
+gpa = int(gpa)
+print(gpa)
+
+#cant convert string to int or float
+
+#bool to int
+student = int(student)
+print(student)
+
+#bool to float
+student = float(student)
+print(student)
+
+#int to bool
+age = bool(age)
+print(age)
+
+#float to bool
+gpa = bool(gpa)
+print(gpa)
+
+#string to bool
+name = bool(name)
+print(name)
+
+#if age = 0, gpa = 0.0 and name = " ", it'll be false
+
 
